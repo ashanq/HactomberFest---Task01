1c6c6de7688c7c07937ac3477558425a905acca1
Amasha Sathsarani <86179556+IT20160098@users.noreply.github.com>
2022-10-22 21:52:18 +0530

    Update DeleteNotice.java
    Fix code errors

--- DeleteNotice.java
+++ DeleteNotice.java
@@ -10,8 +10,8 @@ import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
@@ -23,14 +23,14 @@ import javax.servlet.http.HttpServletResponse;
 */
 @WebServlet("/DeleteNotice")
 public class DeleteNotice extends HttpServlet {
-    private static final long serialVersionUID = 1L
+    private static final long serialVersionUID = 1L;
 
 
 
    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String noticeid = request.getParameter("noticeid");
-        boolean isTrue
+        boolean isTrue;
         
         isTrue = NoticeDBUtil.deletenotice(noticeid);
         
@@ -43,7 +43,7 @@ public class DeleteNotice extends HttpServlet {
             request.setAttribute("noticeDetails", noticeDetails);
             
             RequestDispatcher dispatcher = request.getRequestDispatcher("noticeinsert.jsp");
-            dispatcher.forward(request response);
+            dispatcher.forward(request ,response);
         }
     }
 
