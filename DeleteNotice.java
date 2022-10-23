3282d5b26a6baddc91f21c3626e0cd1478b887a5
Anushka Kiriella <73027430+anuKiriella@users.noreply.github.com>
2022-10-24 01:16:10 +0530

    Update DeleteNotice.java
    Making error in the code

--- DeleteNotice.java
+++ DeleteNotice.java
@@ -10,8 +10,8 @@ import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
@@ -23,27 +23,27 @@ import javax.servlet.http.HttpServletResponse;
 */
 @WebServlet("/DeleteNotice")
 public class DeleteNotice extends HttpServlet {
-    private static final long serialVersionUID = 1L
+    private static final long serialVersionUID = 1L;
 
 
 
    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String noticeid = request.getParameter("noticeid");
-        boolean isTrue
+        boolean isTrue;
         
         isTrue = NoticeDBUtil.deletenotice(noticeid);
         
         if(isTrue == true) {
             RequestDispatcher dispatcher = request.getRequestDispatcher("noticeaccount.jsp");
-            dispatcher.forward(request, response)
+            dispatcher.forward(request, response);
         }
         else {
             List<Notice> noticeDetails = NoticeDBUtil.getNoticeDetails(noticeid);
             request.setAttribute("noticeDetails", noticeDetails);
             
             RequestDispatcher dispatcher = request.getRequestDispatcher("noticeinsert.jsp");
-            dispatcher.forward(request response);
+            dispatcher.forward(request, response);
         }
     }
 
